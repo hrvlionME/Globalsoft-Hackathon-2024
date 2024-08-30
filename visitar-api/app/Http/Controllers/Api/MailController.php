55ab1b94dfc9ebb21c879c0cf7cb01af84db899c
hrvojekozul <hrvlionme@gmail.com>
2024-08-30 16:12:15 +0200

    project setup and mail send working

---
+++ visitar-api/app/Http/Controllers/Api/MailController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\MailRequest;
+use App\Mail\Message;
+use Illuminate\Support\Facades\Mail;
+
+class MailController extends Controller
+{
+    //
+    public function send(MailRequest $request)
+    {
+        Mail::to($request->email)->send(new Message($request->message, $request->name));
+    }
+}
