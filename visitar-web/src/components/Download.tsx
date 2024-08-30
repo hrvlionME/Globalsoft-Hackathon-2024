d017191a02ea4a2a8846e32b8a51c00a5f4574d8
mapokapo <leopetrovic11@gmail.com>
2024-08-30 22:00:39 +0200

    fix: make build info container in Download component a column

--- visitar-web/src/components/Download.tsx
+++ visitar-web/src/components/Download.tsx
@@ -16,20 +16,20 @@ const Download: React.FC = () => {
         <h1 className="relative mx-8 text-center text-6xl uppercase text-white sm:text-6xl md:text-8xl">
           {t("download")}
         </h1>
-        <div className="flex items-center justify-between gap-x-10">
-          <div className="z-10 mt-5 hidden rounded-sm bg-white p-2 md:flex">
-            <QRCodeSVG
-              value={`${window.location.origin}/app.apk`}
-              size={192}
-            />
-          </div>
-          <div className="my-4 flex h-min flex-row justify-center gap-8 rounded-md bg-gradient-to-br from-white/35 to-white/60 px-4 py-2 md:my-0 md:flex-col md:gap-2 md:p-4">
+        <div className="flex flex-col items-center justify-between gap-x-10">
+          <div className="my-4 flex h-min justify-center gap-2 rounded-md bg-gradient-to-br from-white/35 to-white/60 p-4 px-4 py-2">
             <h2 className="text-shadow-sm text-center font-mono text-white shadow-black/85 sm:text-base md:text-2xl">
               {t("app-build")} v1.15
             </h2>
             <h2 className="text-shadow-sm text-center font-mono text-white shadow-black/85 sm:text-base md:text-2xl">
               {format(new Date(), "MMM y")}
             </h2>
+          </div>
+          <div className="z-10 mt-5 hidden rounded-sm bg-white p-2 md:flex">
+            <QRCodeSVG
+              value={`${window.location.origin}/app.apk`}
+              size={192}
+            />
           </div>
         </div>
         <a
