26338b5b7ed48d66a35c70493bad017ab15069aa
mapokapo <leopetrovic11@gmail.com>
2024-08-30 19:09:10 +0200

    chore(i18n): localize ContactUs section

--- visitar-web/src/components/ContactUs.tsx
+++ visitar-web/src/components/ContactUs.tsx
@@ -1,41 +1,61 @@
 import { useState } from "react";
 import axios from "axios";
+import { useTranslation } from "react-i18next";
 const ContactUs: React.FC = () => {
+  const [t] = useTranslation();
 
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
-    const [message, setMessage] = useState('');
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
 
-    const submitData = async () => {
-        console.log(name, email, message);
-        const formData = {
-            name: name,
-            email: email,
-            message: message
-        }
-        try {
-            await axios.post('http://localhost:8000/api/sendMessage', formData);
-            window.location.reload();
-        } catch (error) {
-            console.error('There was an error!', error);
-        }  
-
+  const submitData = async () => {
+    console.log(name, email, message);
+    const formData = {
+      name: name,
+      email: email,
+      message: message,
+    };
+    try {
+      await axios.post("http://localhost:8000/api/sendMessage", formData);
+      window.location.reload();
+    } catch (error) {
+      console.error("There was an error!", error);
     }
-
-    return (
-      <>
-         <section className="bg-pri flex flex-col sm:flex-row justify-around">
-              <div className="text-center lg:mx-auto mx-16 py-12 sm:py-20 lg:py-32 flex flex-col">
-                  <h6 className="text-xl md:text-2xl lg:text-5xl text-white font-title">Have any questions - Contact us here!</h6>
-                  <input type="text" placeholder="Name" className="my-4 text-md md:text-xl px-2 py-3 md:py-4 w-full rounded-lg shadow-md" onChange={(e) => setName(e.target.value)}/>
-                  <input type="text" placeholder="Email" className="my-4 text-md md:text-xl px-2 py-3 md:py-4 w-full rounded-lg shadow-md" onChange={(e) => setEmail(e.target.value)}/>
-                  <textarea className="my-4 text-md md:text-xl px-2 py-3 md:py-4 w-full rounded-lg shadow-md " placeholder="Message" rows={6} onChange={(e) => setMessage(e.target.value)}></textarea>
-                  <button className="my-4 text-md md:text-xl px-2 py-3 md:py-4 w-full rounded-lg shadow-md bg-white text-pri" onClick={submitData}>Submit</button>
-              </div>
-              </section>
-      </>
-    );
   };
-  
-  export default ContactUs;
-  
+
+  return (
+    <>
+      <section className="bg-pri flex flex-col justify-around sm:flex-row">
+        <div className="mx-16 flex flex-col py-12 text-center sm:py-20 lg:mx-auto lg:py-32">
+          <h6 className="font-title text-xl text-white md:text-2xl lg:text-5xl">
+            {t("contact-us.title")}
+          </h6>
+          <input
+            type="text"
+            placeholder={t("name")}
+            className="text-md my-4 w-full rounded-lg px-2 py-3 shadow-md md:py-4 md:text-xl"
+            onChange={e => setName(e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder={t("email")}
+            className="text-md my-4 w-full rounded-lg px-2 py-3 shadow-md md:py-4 md:text-xl"
+            onChange={e => setEmail(e.target.value)}
+          />
+          <textarea
+            className="text-md my-4 w-full rounded-lg px-2 py-3 shadow-md md:py-4 md:text-xl"
+            placeholder={t("message")}
+            rows={6}
+            onChange={e => setMessage(e.target.value)}></textarea>
+          <button
+            className="text-md text-pri my-4 w-full rounded-lg bg-white px-2 py-3 shadow-md md:py-4 md:text-xl"
+            onClick={submitData}>
+            {t("submit")}
+          </button>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default ContactUs;
